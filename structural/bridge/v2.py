744d878ae7fdc3b6f3366fb48ce50e76658d5651
valentynmalyi <valentynmalyi@gmail.com>
2021-10-16 18:22:44 +0300

    add bridge

---
+++ structural/bridge/v2.py
@@ -0,0 +1,44 @@
+class Color:
+    def __init__(self, r: int, g: int, b: int):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def __str__(self):
+        return f"Color(r={self.r}, g={self.g}, b={self.b})"
+
+
+class BlueColor(Color):
+    def __init__(self):
+        super().__init__(0, 0, 255)
+
+
+class RedColor(Color):
+    def __init__(self):
+        super().__init__(255, 0, 0)
+
+
+class YellowColor(Color):
+    def __init__(self):
+        super().__init__(255, 255, 0)
+
+
+class Car:
+    name: str
+
+    def __init__(self, color: Color):
+        self.color = color
+
+    def print(self) -> None:
+        print(f"BMW of color: {self.color}")
+
+
+class Bmw(Car):
+    name = "BMW"
+
+
+class Audi(Car):
+    name = "AUDI"
+
+
+Bmw(RedColor()).print()
