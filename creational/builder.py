d80e973bd81cfb7b2629f5731384056528c7bbf4
valentynmalyi <valentynmalyi@gmail.com>
2021-10-16 15:47:03 +0300

    modify builder

--- creational/builder.py
+++ creational/builder.py
@@ -2,28 +2,28 @@ from __future__ import annotations
 
 
 class Point:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
     def __str__(self):
         return f"({self.x}, {self.y})"
 
+    class Builder:
 
-class PointBuilder:
-    def __init__(self):
-        self.point = Point()
+        def __init__(self):
+            self.point = Point(0, 0)
 
-    def up(self, a: int) -> PointBuilder:
-        self.point.y += a
-        return self
+        def up(self, a: int) -> Point.Builder:
+            self.point.y += a
+            return self
 
-    def right(self, a: int) -> PointBuilder:
-        self.point.x += a
-        return self
+        def right(self, a: int) -> Point.Builder:
+            self.point.x += a
+            return self
 
-    def build(self) -> Point:
-        return self.point
+        def build(self) -> Point:
+            return self.point
 
 
-print(PointBuilder().up(5).right(4).build())
+print(Point.Builder().up(5).right(4).build())
